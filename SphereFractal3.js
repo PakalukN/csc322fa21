6887d8462063a9d4bcfef402d1b9801155af4cf1
PakalukN <72899331+PakalukN@users.noreply.github.com>
2021-09-29 18:05:47 -0400

    Create SphereFractal3.js

---
+++ SphereFractal3.js
@@ -0,0 +1,109 @@
+// initialize
+let renderer, scene, camera, draw, counter = 0;
+function init(){
+	renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+  camera.position.set(0, 0, 100);
+}
+function animate(){
+	requestAnimationFrame(animate);
+  counter++;
+  draw();
+}
+init();
+animate();
+
+
+
+// begin the real stuff
+let color, frac;
+const TAU = 6.283185307179586;
+
+let n = 2, c = 0;
+
+for(let i = 0; i < n; i++){
+	c += Math.pow(n, i);
+}
+
+function init(){
+	// renderer stuff
+	renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  
+  // scene stuff
+  scene = new THREE.Scene();
+  
+  // camera stuff
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+  camera.position.set(0, 0, 100);
+  
+  // set up object
+ 	color = function(r, g, b){
+  	return (r << 16) | (g << 8) | b;
+  }
+  frac = function(x, y, r, m, d, n, i, scene){
+  	let rot = counter / 50.0;
+    //let rot = 5.730;
+    let a = ((2 * r) / (1 + m)) * (1 + m * (1 + Math.sin(rot * 2)) / 2), x1, y1;
+    for(let k = 0; k < n; k++){
+    	x1 = x + Math.cos(d * rot + (k * TAU) / n) * (-r + a / 2);
+      y1 = y + Math.sin(d * rot + (k * TAU) / n) * (-r + a / 2);
+      
+      const geometry = new THREE.SphereGeometry(a, 32, 16);
+      const material = new THREE.MeshStandardMaterial({color: color(255 * i, 255 - 255 * i , 255 * i), roughness: 0.25});
+      const circle = new THREE.Mesh(geometry, material);
+      
+      circle.position.copy(new THREE.Vector3(x1, y1, 50 - 5 * i));
+      
+      /*c++;
+      if(positions.length){
+      	//positions[c] = 
+      }
+      else{*/
+      	scene.add(circle);
+        //positions.push(new THREE.Vector3(x, y, 50));
+      //}
+      if(i > 0){
+        frac(x1, y1, a / 2, m, -d * d, n, i - 1, scene);
+      }
+    }
+	}
+  
+  //var positions = [], c = 0;
+  
+  frac(0, 0, 10, 2.5, 1.25, 5, 1, scene);
+  
+  const l = new THREE.AmbientLight(0x404040);
+	scene.add(l);
+  
+  const dl = new THREE.DirectionalLight(0xffffff, 0.5);
+  dl.position.set(1, 1, 1);
+	scene.add(dl);
+  
+  renderer.render(scene, camera);
+  
+draw = function(){
+  	scene.clear();
+    
+    camera.position.set(0, 0, 100);
+    
+    const l = new THREE.AmbientLight(0x404040);
+		scene.add(l);
+  
+  	const dl = new THREE.DirectionalLight(0xffffff, 0.5);
+  	dl.position.set(1, 1, 1);
+		scene.add(dl);
+    
+    frac(0, 0, 10, 2.5, 1.25, 5, 1, scene);
+    
+    renderer.render(scene, camera);
+  	
+  }
+  
+}
